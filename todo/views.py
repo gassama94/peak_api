6f84c8980d382c3431adaab79b5d42bdbae3a9c9
Saikou Gassama <gassamasaikou@yahoo.com>
2023-11-24 22:28:24 +0000

    Implemented Todo viewset and configured URL routing

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import status
+from .models import Todo
 
 # Create your views here.
+class TodView(viewsets.ModelViewSet):
+  serializer_class = TodoSerializer
+  queryset = Todo.objects.all()
